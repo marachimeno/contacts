6dd5960ec3df085aef549e33b0f54a4511119bd7
Mara Chimeno <marachimeno@gmail.com>
2021-07-25 20:47:33 +0100

    modified migrations to have paper trail before creating contact

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,8 +1,8 @@
 class Contact < ApplicationRecord
+  has_paper_trail
+
   validates :first_name, presence: true, length: { minimum: 2 }
   validates :last_name, presence: true, length: { minimum: 2 }
   validates :email, presence: true, uniqueness: true
   validates :phone_number, presence: true, length: { minimum: 6 }
-
-  has_paper_trail
 end
