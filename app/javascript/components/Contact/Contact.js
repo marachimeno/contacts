5398d03cb75bfda881648bb54d39383d07a286d1
Mara Chimeno <marachimeno@gmail.com>
2021-07-26 12:58:04 +0100

    set up react + react router

---
+++ app/javascript/components/Contact/Contact.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Contact = () => {
+    return(
+        <div>This is the Contacts#show page for our app.</div>
+    )
+}
+
+export default Contact
---
+++ app/javascript/components/Contacts/Contacts.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Contacts = () => {
+    return(
+        <div>This is the Contacts#index page for our app.</div>
+    )
+}
+
+export default Contacts
